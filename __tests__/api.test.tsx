6e3f70a598ab78905fac9da17ab2a2efc02cb2a4
Vickyvackydev <vickyvacky5@gmail.com>
2024-03-24 21:22:23 +0100

    error-fixes

--- __tests__/api.test.tsx
+++ __tests__/api.test.tsx
@@ -2,12 +2,12 @@ import axios from "axios";
 
 it("should return the expected data", async () => {
   const getQuestionEndpoint = "https://qt.organogram.app/questions";
-  const token = process.env.NEXT_PUBLIC_TOKEN; // a token fetched is used for the testing
+  const test_token = "006de34e-5e56-49aa-b680-6dfcba4e12ff"; // a token fetched is used for the testing
 
   try {
     const response = await axios.get(getQuestionEndpoint, {
       headers: {
-        token: token,
+        token: test_token,
       },
     });
 
